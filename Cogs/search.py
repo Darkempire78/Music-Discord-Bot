b7887b42afdd5135fb63ab00afe7a16de5588ebc
Sourcery AI <>
2021-01-09 23:27:53 +0000

    'Refactored by Sourcery'

--- Cogs/search.py
+++ Cogs/search.py
@@ -18,13 +18,11 @@ class CogSearch(commands.Cog):
     async def search(self, ctx, args):
         results = YoutubeSearch(args, max_results=8).to_dict()
         message = ""
-        number = 0
         if len(results) == 0:
             embed=discord.Embed(title="Search results :", description=f"No result found!", color=discord.Colour.random())
             embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
             return await ctx.send(embed=embed)
-        for i in results:
-            number += 1
+        for number, i in enumerate(results, start=1):
             i["title"] =i["title"].replace("*", "\\*")
             message += f"**{number}) ["+ i["title"] + "](https://www.youtube.com"+ i["url_suffix"] + "])** ("+ str(i["duration"]) + ")\n"
         embed=discord.Embed(title="Search results :", description=f"{message}", color=discord.Colour.random())
